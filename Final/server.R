e20507c31138885e5f4729fe43b2756820ecf69d
Robert West <rwest96@live.unc.edu>
2020-07-22 23:14:41 -0400

    Large update to the UI and Server files

--- Final/server.R
+++ Final/server.R
@@ -17,16 +17,45 @@ library(readr)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
     
+    #Read in the data for the analysis
     data = read_csv("Breast_cancer_data.csv")
-    
-    set.seed(122)
-    histdata = rnorm(500)
+    data = data  %>%  mutate(diagnosis = as.factor(diagnosis))
+
     
     output$plot1 = renderPlot({
-        hist(histdata)
+        
+        
+        if(input$plotType == "Bar"){
+            #Create the single Bar plot
+            ggplot(data=data, aes(x=diagnosis))+
+                geom_bar(aes(fill=diagnosis))
+        } else if(input$plotType == "Histogram"){
+            #Create the Histogram plot 
+            if(input$colorCodeHist){
+                ggplot(data=data, aes(x=mean_radius))+
+                    geom_histogram(aes(fill=diagnosis), position="dodge")    
+            } else {
+                ggplot(data=data, aes(x=mean_radius))+
+                    geom_histogram(color="blue", bins = 50)    
+            }
+        } else if(input$plotType == "Scatter"){
+            #Create Scatter Plots
+            if(input$colorCodeScatter){
+                ggplot(data=data, aes_string(x=input$xScatter, y=input$yScatter))+
+                    geom_point(aes(color=diagnosis))
+            } else {
+                ggplot(data=data, aes_string(x=input$xScatter, y=input$yScatter))+
+                    geom_point(color="red")
+            }
+        }   
     })
     
     output$table1 = renderTable({
         head(data)
+    })
+    
+    output$kmeansPlot = renderPlot({
+        ggplot(data=data, aes(x=mean_radius, y=mean_texture))+
+            geom_point(size=3)
     })
 })
--- Final/ui.R
+++ Final/ui.R
@@ -9,13 +9,15 @@ library(tidyverse)
 shinyUI(dashboardPage(
     
     #Title
-    dashboardHeader(title="Data Science project Breast Cancer Data"),
+    dashboardHeader(title="Breast Cancer Data"),
     
     #Define the sidebar menu items
     dashboardSidebar(
         sidebarMenu(
             menuItem("Information", tabName = "info"),
-            menuItem("Summaries", tabName = "summaries")    
+            menuItem("Summaries", tabName = "summaries"), 
+            menuItem("Clustering", tabName = "cluster"),
+            menuItem("Modeling", tabName = "model")
         )
     ), 
     
@@ -24,7 +26,7 @@ shinyUI(dashboardPage(
     dashboardBody(
         tabItems(
             
-            #Info page content
+            #Info Page content
             tabItem(tabName = "info",
                 box(
                     h4("This is a project centered around Breast Cancer Data")
@@ -33,8 +35,75 @@ shinyUI(dashboardPage(
             
             #Summary Page Content
             tabItem(tabName = "summaries",
-                fluidRow(
-                    tableOutput("table1")
+                tabsetPanel(
+                    
+                    #1st tab designated to viewing plots
+                    tabPanel("Plots",
+                        sidebarLayout(
+                            
+                            #Sidebar for selecting type of plot
+                            sidebarPanel(
+                                selectInput("plotType", 
+                                            "Select a Plot",
+                                            choices = c("Bar", "Histogram", "Scatter"), selected = "Scatter"),
+                                conditionalPanel(condition="input.plotType == 'Histogram'",
+                                                 checkboxInput("colorCodeHist", "Color code this Histogram?")),
+                                conditionalPanel(condition="input.plotType == 'Scatter'",
+                                                 selectInput("xScatter", "Choose an X axis",
+                                                             choices = names(data%>%select(-diagnosis)),
+                                                             selected = "mean_radius"),
+                                                 selectInput("yScatter", "Choose a Y axis",
+                                                             choices = names(data%>%select(-diagnosis)),
+                                                             selected = "mean_texture"),
+                                                 checkboxInput("colorCodeScatter", "Color code this Scatter Plot?"))
+                                
+                            ),
+                            
+                            mainPanel(
+                                plotOutput("plot1")    
+                            )
+                        )
+                    ),
+                    
+                    #2nd tab designated to Numeric Summaries
+                    tabPanel("Numeric Summaries",
+                             tableOutput("table1"))
+                )
+            ),
+            
+            #Clustering Page Content
+            tabItem(tabName = "cluster",
+                sidebarLayout(
+                    
+                    #Sidebar for Clustering page
+                    sidebarPanel(
+                        selectInput("xCluster", "Select X axis", 
+                                    choices = names(data %>% select(-diagnosis)), 
+                                    selected = "mean_radius"),
+                        selectInput("yCluster", "Select Y axis",
+                                    choices = names(data %>% select(-diagnosis)),
+                                    selected="mean_texture"),
+                        numericInput("kmeans", "Select K for the K-Means Algorithm", min=1, max=12, step=1, value = 1)
+                    ),
+                    
+                    mainPanel(
+                        plotOutput("kmeansPlot")
+                    )
+                )
+            ),
+            
+            #Modeling Page Content
+            tabItem(tabName = "model",
+                tabsetPanel(
+                    
+                    #1st Tab designated to K Nearest Neighbors
+                    tabPanel("K Nearest Neighbors"),
+                    
+                    #2nd Tab designated to Logistic Regression
+                    tabPanel("Logistic Regression"),
+                    
+                    #3rd Tab designated to Classification Trees
+                    tabPanel("Classification Trees")
                 )
             )
         )
